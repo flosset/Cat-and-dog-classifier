520e7acbfa938eecd2a22c83a5ae235aec68e8d6
Yash Saini <yashsaini@Yashs-MBP.Zyxel.com>
2023-07-22 11:32:22 -0500

    Added frames

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from tkinter import *
 def main():
     root = Tk()
     root.title('Identifier')
-    root.geometry('700x700')
+    root.geometry('800x600')
     
+    # making two frames
+    frame1 = LabelFrame(root, padx=30, pady=30)
+    frame1.grid(row=0, column=0, padx=10, pady=5)
+    
+    frame2 = LabelFrame(root, padx=30, pady=30)
+    frame2.grid(row=0, column=1, padx=10, pady=5)
     
     root.mainloop()
     
