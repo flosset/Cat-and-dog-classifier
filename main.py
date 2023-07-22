0a3daa25e7c1a58fde04557c6e493b02e959b99b
Yash Saini <yashsaini@Yashs-MBP.Zyxel.com>
2023-07-22 14:42:04 -0500

    Saving before implementing code that load datasets

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
 from tkinter import filedialog
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Model, load_model
 
 def main():
+    # create a model to identify cat and dog
+    model = create_model()
+    
     root = Tk()
     root.title('Identifier')
     root.geometry('800x600')
@@ -21,6 +28,16 @@ def main():
     label.pack()
     
     root.mainloop()
+    
+def create_model():
+    '''
+    This model will use skip connections since I am just learning
+    '''
+    X_train, Y_train, X_test, y_test, classes = load_datasets()
+    test = Model()
+    ...
+    
+
     
     
 if __name__ == '__main__':
