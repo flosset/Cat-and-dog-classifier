c85ea15ea519e14639044f6fda93ebf5da97c3e6
Yash Saini <yashsaini@Yashs-MBP.Zyxel.com>
2023-07-22 11:22:53 -0500

    Added basic files

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+def main():
+    root = Tk()
+    root.title('Identifier')
+    root.geometry('700x700')
+    
+    
+    root.mainloop()
+    
+    
+if __name__ == '__main__':
+    main()
